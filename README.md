d522adf5122e1259393c4a5c63cbae19c3ffe4e7
Calder Wilcox <cjw144@georgetown.edu>
2025-06-26 11:51:33 -0400

    Created the initial version

---
+++ README.md
@@ -0,0 +1,10 @@
+Welcome to Find And Replace (FAR)
+This is the first version of a simple linux command line find and replace file
+I am creating this with Linux in mind but hope to expand this to other operating systems soon.
+Each version holds substantial changes to the previous versions, listed below.
+
+Version:
+v1
+    The original version
+v2
+    The vector version
---
+++ far.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+int main() {
+
+//Variable declarations
+
+    std::string userFPath;
+    std::string findWord;
+    std::string replaceWord;
+    std::string outputFPath = "./out.txt";
+    std::string word;
+    std::string fileWord;
+    int wordCount = 0;
+    int replaceCount = 0;
+
+    //Getting user input
+    std::cout << "Welcome to F.A.R." << std::endl;
+    std::cout << "File path: ";
+    std::cin >> userFPath;
+    std::cout << "Output Path: ";
+    std::cin >> outputFPath;
+    std::cout << "Find: ";
+    std::cin >> findWord;
+    std::cout << "Replace: ";
+    std::cin >> replaceWord;
+
+    //Opening and checking the validity of the user provided file
+    std::ifstream inputFile;
+    inputFile.open(userFPath);
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: Could not open input file." << std::endl;
+        return 0;
+        
+    }
+
+    //Opening and checking the validity of the output file
+    std::ofstream outputFile;
+    outputFile.open(outputFPath);
+
+    if (!outputFile.is_open()) {
+        std::cerr << "Error: Could not open output file." << std::endl;
+        return 0;
+    }
+
+    //Finding and replacing
+    while (inputFile >> fileWord) {
+    if (fileWord == findWord) {
+        std::cout << " " << replaceWord;
+        replaceCount++;
+        wordCount++;
+    } else {
+        std::cout << " " << fileWord;
+        wordCount++;
+        }
+    }
+    inputFile.close();
+    outputFile.close();
+
+    std::cout << "Process Compleat!" << std::endl;
+    std::cout << "Total Words Checked: " << wordCount << std::endl;
+    std::cout << "Total Words Replaced: " << replaceCount << std::endl;
+    std::cout << "The output file is located at: " << outputFPath << std::endl;
+    return 0;
+}
